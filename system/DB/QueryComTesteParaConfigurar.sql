5483b525047811baefd101350325789e18a972c3
KauanTkn <takakaiky@gmail.com>
2024-11-11 11:48:13 -0300

    :sparkles: feat: Adicionado query para Configuração

---
+++ system/DB/QueryComTesteParaConfigurar.sql
@@ -0,0 +1,16 @@
+-- Deve estar Inserido os cadastros para rodar
+
+CREATE TABLE IF NOT EXISTS expediente (
+    id_expediente INT AUTO_INCREMENT PRIMARY KEY,
+    inicio_expediente TIME NOT NULL,
+    fim_expediente TIME NOT NULL,
+    duracao_intervalo TIME NOT NULL,
+    latitude DECIMAL(9,6) NOT NULL,
+    longitude DECIMAL(9,6) NOT NULL
+);
+
+INSERT INTO expediente (inicio_expediente, fim_expediente, duracao_intervalo, latitude, longitude) 
+VALUES ('08:00:00', '17:00:00', '01:00:00', -23.550520, -46.633308);
+
+SELECT * FROM expediente;
+SHOW TABLES;
